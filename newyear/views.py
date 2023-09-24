b1f87e67a0f20e1d6e2f3b618960c5a0aea509b2
jurco-exe <jurajbosy1@gmail.com>
2023-09-24 19:05:00 +0200

    feat: added static styles into newyear template

--- newyear/views.py
+++ newyear/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 import datetime
 
-
-# Create your views here.
 def index(request):
   now = datetime.datetime.now()
   return render(request, "newyear/index.html", {
