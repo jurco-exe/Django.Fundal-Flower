48db099dc4ada1fb83e17cccc0f5a562895322ed
jurco-exe <jurajbosy1@gmail.com>
2023-09-30 02:12:42 +0200

    feat: POST request FORM + sessions + adding tasks...

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+class NewTaskForm(forms.Form):
+  task = forms.CharField(label="New Task")
+  # priority = forms.IntegerField(label="Priority", min_value = 1, max_value= 5)
 
 # Create your views here.
 def index(request):
+  if "tasks" not in request.session:
+    request.session["tasks"] = []
+
   return render(request, "tasks/index.html", {
-    "tasks": tasks
+    "tasks": request.session["tasks"]
   })
 
 def add(request):
-  return render(request, "tasks/add.html")
+
+  # Request Method Check
+  if request.method == "POST":
+    # Form Handling
+    # this variable will now hold every vbalue that user submit
+    form = NewTaskForm(request.POST)
+    if form.is_valid():
+      task = form.cleaned_data["task"]
+      request.session["tasks"] += [task]
+      return HttpResponseRedirect(reverse("tasks:index"))
+
+    else:
+      return render(request, 'tasks/add.html', {
+        "form": form
+      })
+
+  return render(request, "tasks/add.html", {
+    "form": NewTaskForm()
+  })
